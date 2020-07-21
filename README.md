02f4180b7949fc945a62e1ca74516fd809848bb4
Aleksandr Timoschenko <timoschenkoaa@okbtsp.com>
2020-07-21 16:03:44 +0300

    Cloud bastion: fix setupvpn.sh, README.md

--- README.md
+++ README.md
@@ -3,16 +3,20 @@ timoschenkoaa Infra repository
 
 ДЗ №3 Запуск VM в YC
 Задание
+
 bastion_IP = 84.201.135.97
 someinternalhost_IP = 10.130.0.16
 
 Самостоятельное задание
+
 ssh -i ~/.ssh/id_rsa -J appuser@84.201.173.21 appuser@10.130.0.22
 
 Дополнительное задание
+
 В конфиг нужно прописать хост bastion и someinternalhost с ProxyJump.
 
 Host someinternalhost
+
   HostName 10.130.0.22
   ForwardAgent yes
   ProxyJump  appuser@84.201.173.21
--- setupvpn.sh
+++ setupvpn.sh
@@ -3,7 +3,7 @@ echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" 
 echo "deb http://repo.pritunl.com/stable/apt xenial main" > /etc/apt/sources.list.d/pritunl.list
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv E162F504A20CDF15827F718D4B7C549A058F8B6B
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
-sudo apt-get --assume-yes install apt-transport-https
+sudo apt-get --assume-yes install apt-transport-https iptables iptables-persistent
 sudo apt-get update
 sudo apt-get --assume-yes install pritunl mongodb-org
 sudo systemctl start pritunl mongod
