0a0804cdaeddfa08f2573b7d034788255e71faa2
Aleksandr Timoschenko <timoschenkoaa@okbtsp.com>
2020-07-24 15:23:58 +0300

    Cloud testapp: fix *.sh

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -7,4 +7,3 @@ echo "deb [ arch=amd64,arm64] https://repo.mongodb.org/apt/ubuntu xenial/mongodb
 sudo apt update && sudo apt install -y mongodb-org
 sudo systemctl start mongod
 sudo systemctl enable mongod
-sudo status mongod
